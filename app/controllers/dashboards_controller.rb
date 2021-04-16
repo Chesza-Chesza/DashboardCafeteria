4457b03447b1e8b1ad4cff66a8f65aae894f5ffc
Shirley Alvarado <shirley@codecrc.com>
2021-04-16 09:36:14 -0600

    Graphics done

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,18 @@
 class DashboardsController < ApplicationController
   def index
-    @coffees_in_months = Coffee.group_by_month(:created_at).count
-  end
+    @coffees = Coffee.all
+    @sales_12_months = Coffee.group_by_month(:created_at, last:12).sum(:price)
+    @quantity_12_months = Coffee.group_by_month(:created_at, last:12).sum(:amount)
+    @average_12_months = Coffee.group_by_month(:created_at, last:12).average(:amount)
+    
+    @pie_chart_12_months = Coffee.where(created_at: 5.months.ago..Date.today).group(:origin).count
+    @pie_chart_6_months = Coffee.where(created_at: 5.months.ago..Date.today).group(:origin).count
+    @pie_chart_3_months = Coffee.where(created_at: 2.months.ago..Date.today).group(:origin).count
+    @pie_chart_last_month = Coffee.where(created_at: 30.days.ago..Date.today).group(:origin).count
+    
+    @pie_blend_name_12_months = Coffee.where(created_at: 11.months.ago..Date.today).group(:blend).count
+    @pie_blend_name_6_months = Coffee.where(created_at: 5.months.ago..Date.today).group(:blend).count  
+    @pie_blend_name_3_months = Coffee.where(created_at: 2.months.ago..Date.today).group(:blend).count
+    @pie_blend_name_last_month = Coffee.where(created_at: 30.days.ago..Date.today).group(:blend).count
+  end  
 end
